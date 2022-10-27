eac0e2b095c7199bfe747f77d5b0208fd4cfbf17
Lucas Aleck <anon>
2022-10-27 18:12:01 -0500

    kills working

--- checkers.py
+++ checkers.py
@@ -48,7 +48,19 @@ class Coordinate():
         """need this when checking if coordinate in list of candidate coordinates """
         return self.r == obj.r and self.c == obj.c
 
-    def check_valid(self) -> bool:
+    def is_simple_move(self, obj) -> int:
+        """if row differs by 1, simple jump"""
+        return abs(self.r - obj.r) == 1
+
+    def is_kill_move(self, obj) -> int:
+        """if row differs by 2, kill"""
+        return abs(self.r - obj.r) == 2
+
+    def get_kill_coordinate(self, obj):
+        """return coordinate of the leapfrogged space"""
+        return Coordinate((self.r + obj.r) // 2, (self.c + obj.c) // 2)
+
+    def is_inbounds(self) -> bool:
         """checks if coordinates is in bounds of board"""
         return self.r >= 0 and self.r < ROWS and self.c >= 0 and self.c < COLS
         
@@ -101,9 +113,18 @@ class Board:
     
     def place(self, piece: Optional[Piece], coord: Coordinate) -> bool:
         """ place a Piece/None on coordinate"""
-        if not self.board[coord.r][coord.c]:
+        if not self.at(coord):
             # if empty (None)
             self.board[coord.r][coord.c] = piece
+            return True
+        
+        return False
+
+    def remove(self, coord: Coordinate) -> bool:
+        """ place a Piece/None on coordinate"""
+        if self.at(coord):
+            # if something at this spot
+            self.board[coord.r][coord.c] = None
             return True
         
         return False
@@ -119,56 +140,89 @@ class Board:
         if new_coord in candidates:
             # place piece on new empty spot
             self.place(piece, new_coord)
-
             # remove piece from last spot
-            self.place(None, old_coord)
+            self.remove(old_coord)
+
+            if new_coord.is_kill_move(old_coord):
+                # nullify killed spot
+                self.remove(new_coord.get_kill_coordinate(old_coord))
 
             return True
 
         return False
-
+    
     def list_moves(self, coord: Coordinate) -> list[Coordinate]:
         """
         Return list of possible coordinates
         New coordinate space must be
-            - in bounds
-            - empty
+            1)
+             - in bounds
+            2)
+             - adjacent diagonal space empty (simple move)
+             or
+             - adjacent diagonal occupied by enemy and subsequent diagnoal empty (kill)
         """
-        moves = []
+        candidate_coords = []
         piece = self.at(coord)
         if not piece:
-            return moves
+            return candidate_coords
 
         if piece.direction == Direction.UP:
 
-            up_left = coord.add(Move.UP_LEFT)
-            if up_left.check_valid() and not self.at(up_left):
-                moves.append(up_left)
+            candidate = self.list_move_helper(coord, Move.UP_LEFT)
+            if candidate:
+                candidate_coords.append(candidate)
 
-            up_right = coord.add(Move.UP_RIGHT)
-            if up_right.check_valid() and not self.at(up_right):
-                moves.append(up_right)
+            candidate = self.list_move_helper(coord, Move.UP_RIGHT)
+            if candidate:
+                candidate_coords.append(candidate)
 
         elif piece.direction == Direction.DOWN:
 
-            down_left = coord.add(Move.DOWN_LEFT)
-            if down_left.check_valid() and not self.at(down_left):
-                moves.append(down_left)
+            candidate = self.list_move_helper(coord, Move.DOWN_LEFT)
+            if candidate:
+                candidate_coords.append(candidate)
 
-            down_right = coord.add(Move.DOWN_RIGHT)
-            if down_right.check_valid() and not self.at(down_left):
-                moves.append(down_right)
+            candidate = self.list_move_helper(coord, Move.DOWN_RIGHT)
+            if candidate:
+                candidate_coords.append(candidate)
 
         else:
             # log an error
             print('* ERROR: piece at {}, {} has no direction'.format(coord.r, coord.c))
 
-        return moves
+        return candidate_coords
+
+    def list_move_helper(self, coord: Coordinate, move: Move) -> Optional[Coordinate]:
+        new_coord = coord.add(move)
+        if new_coord.is_inbounds():
+
+            if not self.at(new_coord): # simple move into an empty spot
+                return new_coord
+
+            # board new_coord occupied by a non null piece
+            elif self.at(new_coord).team != self.at(coord).team: # spot occupied by enemy, jump and kill it
+                new_coord = new_coord.add(move)
+                if new_coord.is_inbounds() and not self.at(new_coord): # can jump into empty spot and kill
+                    return new_coord
+            else:
+                print("no conditions satisfied")
+
+        return None
 
 
-board = Board()
-piece = Piece(Team.X, Direction.DOWN)
-board.place(piece, Coordinate(0, 1))
-print(board)
-moves = board.list_moves(Coordinate(0, 1))
-print(moves)
+if __name__ == '__main__':
+    def example_kills():
+        print(board)
+        board.move(Coordinate(2,1), Coordinate(3,2))
+        print(board)
+        board.move(Coordinate(3,2), Coordinate(4,1))
+        print(board)
+        board.move(Coordinate(5,0), Coordinate(3, 2))
+        print(board)
+        board.move(Coordinate(2,3), Coordinate(4, 1))
+        print(board)
+        board.move(Coordinate(5,2), Coordinate(3, 0))
+        print(board)
+
+    example_kills()
