34944829e5527ad1b20096024102ce190580139c
Lucas Aleck <anon>
2022-10-27 16:21:23 -0500

    fixed elgible moves. still need to implement king and kill

--- checkers.py
+++ checkers.py
@@ -9,7 +9,6 @@ from typing import *
 |██O ██O ██O ██O |
 |O ██O ██O ██O ██|
 
-1. For some arbitrary game state of a checkers game, return all possible next moves.
 '''
 ROWS = 8
 COLS = 8
@@ -42,13 +41,15 @@ class Coordinate():
     def add(self, move: Move):
         return Coordinate(self.r + move.value[0].value, self.c + move.value[1].value)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return 'r: {}, c: {}'.format(self.r, self.c)
 
     def __eq__(self, obj):
+        """need this when checking if coordinate in list of candidate coordinates """
         return self.r == obj.r and self.c == obj.c
 
     def check_valid(self) -> bool:
+        """checks if coordinates is in bounds of board"""
         return self.r >= 0 and self.r < ROWS and self.c >= 0 and self.c < COLS
         
 
@@ -57,12 +58,12 @@ class Piece:
         self.team = team
         self.direction = direction
     
-    def __str__(self):
+    def __str__(self) -> str:
         return self.team.value
     
 class Board:
     """
-    Origin is top left corner at 0,0
+    Origin is top left corner at r=0, c=0
     """
     def __init__(self):
         # board filled with no pieces
@@ -72,30 +73,34 @@ class Board:
         for r in range(3):
             start = 1 if r % 2 == 0 else 0
             for c in range(start, COLS, 2):
-                self.board[r][c] = Piece(Team.X, Direction.DOWN)
+                self.place(Piece(Team.X, Direction.DOWN), Coordinate(r, c))
 
         # board filed with O's going up
         for r in range(COLS-1, COLS-4, -1):
             start = 1 if r % 2 == 0 else 0
             for c in range(start, COLS, 2):
-                self.board[r][c] = Piece(Team.O, Direction.UP)
-        
-    def __str__(self):
-        result = ''
-        for i in range(ROWS):
-            for j in range(COLS):
+                self.place(Piece(Team.O, Direction.UP), Coordinate(r, c))
 
-                if self.board[i][j]:
-                    result += str(self.board[i][j])
+        
+    def __str__(self) -> str:
+        result = ''
+        for r in range(ROWS):
+            for c in range(COLS):
+
+                if self.board[r][c]:
+                    result += str(self.board[r][c])
                 else:
                     result += Team.EMPTY.value
 
             result += '\n'
 
         return result
+
+    def at(self, coord: Coordinate) -> Optional[Piece]:
+        return self.board[coord.r][coord.c]
     
-    def place(self, piece: Piece, coord: Coordinate) -> bool:
-        """ place a piece on coordinate. use primarily for testing"""
+    def place(self, piece: Optional[Piece], coord: Coordinate) -> bool:
+        """ place a Piece/None on coordinate"""
         if not self.board[coord.r][coord.c]:
             # if empty (None)
             self.board[coord.r][coord.c] = piece
@@ -104,38 +109,55 @@ class Board:
         return False
 
     def move(self, old_coord: Coordinate, new_coord: Coordinate) -> bool:
-        piece = self.board[old_coord.r][old_coord.c]
+        """
+        move piece from a to b
+        """
+        
+        piece = self.at(old_coord)
         candidates = self.list_moves(old_coord)
 
         if new_coord in candidates:
-            # place piece on new spot
-            self.board[new_coord.r][new_coord.c] = piece
+            # place piece on new empty spot
+            self.place(piece, new_coord)
 
             # remove piece from last spot
-            self.board[old_coord.r][old_coord.c] = None
+            self.place(None, old_coord)
 
             return True
 
         return False
 
     def list_moves(self, coord: Coordinate) -> list[Coordinate]:
-        """ Return list of possible coordinates """
+        """
+        Return list of possible coordinates
+        New coordinate space must be
+            - in bounds
+            - empty
+        """
         moves = []
-        piece = self.board[coord.r][coord.c]
+        piece = self.at(coord)
         if not piece:
             return moves
 
         if piece.direction == Direction.UP:
-            if coord.add(Move.UP_LEFT).check_valid():
-                moves.append(coord.add(Move.UP_LEFT))
-            if coord.add(Move.UP_RIGHT).check_valid():
-                moves.append(coord.add(Move.UP_RIGHT))
+
+            up_left = coord.add(Move.UP_LEFT)
+            if up_left.check_valid() and not self.at(up_left):
+                moves.append(up_left)
+
+            up_right = coord.add(Move.UP_RIGHT)
+            if up_right.check_valid() and not self.at(up_right):
+                moves.append(up_right)
 
         elif piece.direction == Direction.DOWN:
-            if coord.add(Move.DOWN_LEFT).check_valid():
-                moves.append(coord.add(Move.DOWN_LEFT))
-            if coord.add(Move.DOWN_RIGHT).check_valid():
-                moves.append(coord.add(Move.DOWN_RIGHT))
+
+            down_left = coord.add(Move.DOWN_LEFT)
+            if down_left.check_valid() and not self.at(down_left):
+                moves.append(down_left)
+
+            down_right = coord.add(Move.DOWN_RIGHT)
+            if down_right.check_valid() and not self.at(down_left):
+                moves.append(down_right)
 
         else:
             # log an error
